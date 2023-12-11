671b396de03f414fab5532608285eb45b6e34c29
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-12-11 19:18:59 +0600

    implements counter successfully

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  let [counter, setCounter] = useState(15);
+
+  const addValue = () => {
+    if (counter < 20) {
+      counter = counter + 1;
+      setCounter(counter);
+    }
+  };
+
+  const removeValue = () => {
+    if (counter > 0) {
+      counter = counter - 1;
+      setCounter(counter);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <h1>Counter Value: {counter}</h1>
+        <button onClick={addValue}>Add Value:{counter} </button>
+        <br />
+        <button onClick={removeValue}>Remove Value: {counter} </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
