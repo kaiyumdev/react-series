9d345a3b2f53cb8f923ac5c38193b8e060b51212
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-12-13 01:53:19 +0600

    implements useState successfully for passwordgenerator

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,12 +1,21 @@
 /* eslint-disable no-unused-vars */
 import { useState } from "react";
 import "./App.css";
+import { useCallback } from "react";
 
 function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxy";
+
+    if(numberAllowed) str +="123456789"
+    if(charAllowed) str +="!@#$^&*-_=+{}[]~`"
+  },[length, numberAllowed, charAllowed, setPassword])
 
   return (
     <>
