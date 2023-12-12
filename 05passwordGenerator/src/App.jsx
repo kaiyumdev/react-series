06255ce604f9bdb7644c5e8ea863f722f8b7011b
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-12-13 02:48:40 +0600

    implements tailwindcss successfully

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -28,7 +28,17 @@ function App() {
 
   return (
     <>
-      <h1 className="text-4xl text-center text-white ">Password Generator</h1>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-800 ">
+        <h1 className="text-white text-center my-3">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4 ">
+          <input
+            type="text"
+            value={password}
+            placeholder="password"
+            className="outline-none w-full py-1 px-3 readonly"
+          />
+        </div>
+      </div>
     </>
   );
 }
