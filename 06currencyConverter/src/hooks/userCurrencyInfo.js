db43c8de200f024d7c99eea7de2b6be712f01153
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-12-15 18:25:59 +0600

    fetch api by using customhooks successfully

---
+++ 06currencyConverter/src/hooks/userCurrencyInfo.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+/* eslint-disable no-unused-vars */
+function userCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+  }, [currency]);
+}
+
+export default userCurrencyInfo;
