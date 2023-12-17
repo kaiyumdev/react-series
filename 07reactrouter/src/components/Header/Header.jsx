e1cc36be4efba44e4c223d5df1a1852a7e4b4ede
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-12-17 23:26:09 +0600

    successfully solve some footer issues problem

--- 07reactrouter/src/components/Header/Header.jsx
+++ 07reactrouter/src/components/Header/Header.jsx
@@ -49,10 +49,10 @@ export const Header = () => {
               <li>
                 <NavLink
                   to="/about"
-                  className={(isActive) =>
-                    `block py-2 pr-4 pl-3 ${
+                  className={({ isActive }) =>
+                    `block py-2 pr-4 pl-3 duration-200 ${
                       isActive ? "text-orange-700" : "text-gray-700"
-                    } duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                    }  border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                   }
                 >
                   About
