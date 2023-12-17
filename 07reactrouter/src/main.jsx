6a76e218339ad57b754d2e8664e158b7546a8c71
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-12-18 00:37:22 +0600

    get github followers & picture by using loader router methods

--- 07reactrouter/src/main.jsx
+++ 07reactrouter/src/main.jsx
@@ -13,7 +13,7 @@ import { Home } from "./components/Home/Home.jsx";
 import { About } from "./components/About/About.jsx";
 import { Contact } from "./components/Contact/Contact.jsx";
 import { User } from "./components/User/User.jsx";
-import { Github } from "./components/Github/Github.jsx";
+import { Github, githubInfoLoader } from "./components/Github/Github.jsx";
 
 // const router = createBrowserRouter([
 //   {
@@ -42,7 +42,11 @@ const router = createBrowserRouter(
       <Route path="" element={<Home></Home>}></Route>
       <Route path="about" element={<About></About>}></Route>
       <Route path="contact" element={<Contact></Contact>}></Route>
-      <Route path="github" element={<Github></Github>}></Route>
+      <Route
+        loader={githubInfoLoader}
+        path="github"
+        element={<Github></Github>}
+      ></Route>
       <Route path="user/:userId" element={<User></User>}></Route>
     </Route>
   )
