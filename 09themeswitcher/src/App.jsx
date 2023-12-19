156dcef8cc649a601c02878b0dfebe7150796c82
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-12-20 00:57:58 +0600

    updated context api successfully

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -1,6 +1,4 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-/* eslint-disable no-undef */
-/* eslint-disable no-unreachable */
 import "./App.css";
 import Card from "./components/Card";
 import ThemeBtn from "./components/ThemeBtn";
@@ -25,21 +23,22 @@ useEffect(() => {
 }, [themeMode]);
 
 function App() {
-  return;
-  <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
-    <div className="flex flex-wrap min-h-screen items-center">
-      <div className="w-full">
-        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-          <ThemeBtn></ThemeBtn>
-        </div>
+  return (
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn></ThemeBtn>
+          </div>
 
-        <div className="w-full max-w-sm mx-auto">
-          <Card></Card>
+          <div className="w-full max-w-sm mx-auto">
+            <Card></Card>
+          </div>
         </div>
       </div>
-    </div>
-    ;
-  </ThemeProvider>;
+      ;
+    </ThemeProvider>
+  );
 }
 
 export default App;
