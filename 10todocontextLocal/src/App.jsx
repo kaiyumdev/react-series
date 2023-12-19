435c59c31f3416f78ec5e66cc8c0c9a2cba3edf9
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-12-20 04:12:14 +0600

    updated addTodo successfully

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -1,9 +1,19 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 import "./App.css";
 import { TodoProvider } from "./context";
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
   return (
-    <TodoProvider>
+    <TodoProvider
+      value={{ todos, addTodo, updatedTodo, deleteTodo, toggleComplete }}
+    >
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
