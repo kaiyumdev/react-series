f216c88a39822124b89efa08b72533eabf21bed3
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-12-20 19:07:48 +0600

    TodoContext componont build by won

--- 10todocontextLocal/src/context/TodoContext.js
+++ 10todocontextLocal/src/context/TodoContext.js
@@ -1,17 +1,39 @@
 /* eslint-disable no-unused-vars */
-import { createContext } from "react";
-import { useContext } from "react";
+// import { createContext } from "react";
+// import { useContext } from "react";
+// export const TodoContext = createContext({
+//   todos: [
+//     {
+//       id: "1",
+//       todo: "Todo msg",
+//       completed: false,
+//     },
+//   ],
+//   addTodo: (todo) => {},
+//   updateTodo: (id, todo) => {},
+//   deleteTodo: (id, todo) => {},
+//   toggleComplete: () => {},
+// });
+
+import { createContext, useContext } from "react";
+
+// export const useTodo = () => {
+//   return useContext(TodoContext);
+// };
+
+// export const TodoProvider = TodoContext.Provider;
+
 export const TodoContext = createContext({
   todos: [
     {
       id: "1",
-      todo: "Todo msg",
+      todo: "Todo Msg",
       completed: false,
     },
   ],
   addTodo: (todo) => {},
   updateTodo: (id, todo) => {},
-  deleteTodo: (id, todo) => {},
+  deleteTodo: (id) => {},
   toggleComplete: () => {},
 });
 
