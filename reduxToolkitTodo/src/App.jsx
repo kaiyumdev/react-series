d3f8f41466bf276fd3d9e7459719dea78693d323
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-12-21 13:01:39 +0600

    complete project successfully

--- reduxToolkitTodo/src/App.jsx
+++ reduxToolkitTodo/src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 
 function App() {
   return (
     <>
       <h1>Redux</h1>
+      <AddTodo></AddTodo>
+      <Todos></Todos>
     </>
   );
 }
